5ca5d78b07501794a0a20b939151a9583666a62c
ethanclawsie <ethanclawsie@gmail.com>
2022-11-03 21:10:20 -0700

    Added node

--- index.js
+++ index.js
@@ -16,52 +16,48 @@ y4.innerHTML = year4;
 y5 = document.getElementById("y5");
 y5.innerHTML = year5;
 
-
 function handleCredentialResponse(response) {
-    const dataToken = JSON.parse(atob(response.credential.split('.')[1]));
-    email = dataToken.email;
-    if (email.substring(email.indexOf('@')) != "@scu.edu") {
-        document.getElementById("emailErr").innerHTML += "You must use an SCU email to register an account.";
-        document.getElementById("emailErr").style.visibility = "visible";
-    }
-  
-    else {
-        document.getElementById("emailErr").style.visibility = "hidden";
-        const dbref = window.moduleRef(window.database);
-        window.moduleGet(window.modChild(dbref, "users/" + email.substring(0, email.indexOf('@')))).then((snapshot) => {
-            if (snapshot.exists()) {
-                alert("You have an account");
-                //sign them in
-            }
-            else {
-                document.getElementById("accountForm").style.visibility = "visible";
-                createAccount(dataToken.email);
-                window.createUser(auth, email);
-            }
-        });
-        
-    }
+  const dataToken = JSON.parse(atob(response.credential.split(".")[1]));
+  email = dataToken.email;
+  if (email.substring(email.indexOf("@")) != "@scu.edu") {
+    document.getElementById("emailErr").innerHTML +=
+      "You must use an SCU email to register an account.";
+    document.getElementById("emailErr").style.visibility = "visible";
+  } else {
+    document.getElementById("emailErr").style.visibility = "hidden";
+    const dbref = window.moduleRef(window.database);
+    window
+      .moduleGet(
+        window.modChild(
+          dbref,
+          "users/" + email.substring(0, email.indexOf("@"))
+        )
+      )
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          alert("You have an account");
+          //sign them in
+        } else {
+          document.getElementById("accountForm").style.visibility = "visible";
+          createAccount(dataToken.email);
+          window.createUser(auth, email);
+        }
+      });
+  }
 }
 
-function createAccount(email) {
-    
-    
-    
-
-}
+function createAccount(email) {}
 
 function addUserToDataBase() {
-   
-    document.getElementById("accountForm").style.visibility = "hidden";
-    let id = email.substring(0, email.indexOf('@'));
-    userYear = document.getElementById('year').value;
-    fName = document.getElementById('firstName').value;
-    lName = document.getElementById('lastName').value;
-    window.moduleSet(window.moduleRef(window.database, 'users/' + id), {
-        userEmail: email,
-        firstName: fName,
-        lastName: lName,
-        year: userYear
-    });
-
+  document.getElementById("accountForm").style.visibility = "hidden";
+  let id = email.substring(0, email.indexOf("@"));
+  userYear = document.getElementById("year").value;
+  fName = document.getElementById("firstName").value;
+  lName = document.getElementById("lastName").value;
+  window.moduleSet(window.moduleRef(window.database, "users/" + id), {
+    userEmail: email,
+    firstName: fName,
+    lastName: lName,
+    year: userYear,
+  });
 }
