c105509a1d92d6eebeef99ff5e62a0807b5b9816
Stevie Dean <stephenwdean@gmail.com>
2022-11-06 23:03:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function login() {
       // The email of the user's account used.
       //const email = error.customData.email;
       // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
+      const credential = window.gap.credentialFromError(error);
       // ...
     });
 }
