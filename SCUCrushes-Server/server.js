79b298df030f715dad6ee115df3c2bd89b5a08ac
Stevie Dean <stephenwdean@gmail.com>
2022-11-06 21:28:16 -0800

    fart

--- SCUCrushes-Server/server.js
+++ SCUCrushes-Server/server.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.post("/sendx", (req, res) => {
   console.log(req.body.x);
   //"Printing" the information sent from the web
-  const x = "testing";
-  res.send({ x });
+  //const x = "testing";
+  const a = req.body.x;
+  res.send({a});
   //Sending a response to the web
 });
