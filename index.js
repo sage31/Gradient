b8d7e83798fc93b06b1a3b1bedefb9f5b295b4d4
Stevie Dean <stephenwdean@gmail.com>
2022-11-09 21:13:29 -0800

    testing something

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ function login() {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = window.gap.credentialFromResult(result);
       const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      console.log(result);
+      
       sendx(result);
       /*
       
