5463ebbf6376d03cecb404f5229669fa61475c99
Stevie Dean <stephenwdean@gmail.com>
2022-11-22 16:59:36 -0800

    cleaned up index.js, moving code to module

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let year2 = year1 + 1;
 let year3 = year2 + 1;
 let year4 = year3 + 1;
 let year5 = year4 + 1;
-var data;
 
 y1 = document.getElementById("y1");
 y1.innerHTML = year1;
@@ -15,46 +14,6 @@ y4 = document.getElementById("y4");
 y4.innerHTML = year4;
 y5 = document.getElementById("y5");
 y5.innerHTML = year5;
-
-function login() {
-  window
-    .signIn(window.auth, window.provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = window.gap.credentialFromResult(result);
-      const token = credential.accessToken;
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      //const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = window.gap.credentialFromError(error);
-      // ...
-    });
-}
-
-function addUser() {
-  //user id/email will be set to a global variable in server
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var year = document.getElementById("year").value;
-
-  if (firstName == "" || lastName == "" || year == "Select Year") {
-    alert("Please fill out all fields");
-  } else {
-    //send data here
-    let data = {
-      uid: window.uid,
-      fName: firstName,
-      lName: lastName,
-      gradYear: year,
-    };
-    sendData(data);
-  }
-}
 
 function sendData(data) {
   fetch("https://SCUCrushes-Server.ethancl.repl.co/sendData", {
