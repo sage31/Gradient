be57bbf24a1b9d7518b442722caf9d3f06b2e070
Stevie Dean <stephenwdean@gmail.com>
2022-11-25 10:59:32 -0800

    working on addCrush button

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 //onload call opendata
 window.onload = keeploggedin;
+let year1 = new Date().getFullYear();
+let year2 = year1 + 1;
+let year3 = year2 + 1;
+let year4 = year3 + 1;
+let year5 = year4 + 1;
+y1 = document.getElementById("y1");
+y1.innerHTML = year1;
+y2 = document.getElementById("y2");
+y2.innerHTML = year2;
+y3 = document.getElementById("y3");
+y3.innerHTML = year3;
+y4 = document.getElementById("y4");
+y4.innerHTML = year4;
+y5 = document.getElementById("y5");
+y5.innerHTML = year5;
+
 
 function keeploggedin() {
   //Store the string "hello" in the local storage
